855f6d6725503bc9c75a29ecc6246e4b3ca4b15b
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2024-10-15 04:36:35 +0200

    character selection autocomplete

---
+++ frontend/app/chat/page.tsx
@@ -0,0 +1,9 @@
+import { title } from "@/components/primitives";
+
+export default function ChatPage() {
+  return (
+    <div>
+      <h1 className={title()}>Chat</h1>
+    </div>
+  );
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,29 +1,36 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import {
-  Autocomplete,
-  AutocompleteItem,
-  Avatar,
-  Button,
-} from "@nextui-org/react";
+import { Autocomplete, AutocompleteItem, Avatar } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 
 import { SearchIcon } from "@/components/icons";
 import { Character } from "@/types";
 
+function truncateString(text: string, maxLength: number = 150): string {
+  let truncated = text.substring(0, maxLength);
+
+  if (text.length > maxLength) {
+    truncated += "...";
+  }
+
+  return truncated;
+}
+
 export default function Home() {
+  const router = useRouter();
   const [characters, setCharacters] = useState<Character[]>();
 
   useEffect(() => {
     fetch(
-      "http://localhost:8080/characters?columns=id&columns=summary&columns=image_url",
+      "http://localhost:8080/characters?columns=id&columns=summary&columns=image_url&columns=name&sort_by=relevance",
     ).then((response) => response.json().then((data) => setCharacters(data)));
   }, []);
 
   return characters ? (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="w-max text-center justify-center">
+      <div className="min-w-96 text-center justify-center">
         <Autocomplete
-          aria-label="Select an employee"
+          aria-label="Select a character"
           classNames={{
             base: "max-w-xs",
             listboxWrapper: "max-h-[320px]",
@@ -52,7 +59,7 @@ export default function Home() {
               ],
             },
           }}
-          placeholder="Enter employee name"
+          placeholder="Select a character"
           popoverProps={{
             offset: 10,
             classNames: {
@@ -71,7 +78,11 @@ export default function Home() {
           variant="bordered"
         >
           {(character) => (
-            <AutocompleteItem key={character.id!} textValue={character.name}>
+            <AutocompleteItem
+              key={character.id!}
+              textValue={character.name}
+              onClick={() => router.push("/chat")}
+            >
               <div className="flex justify-between items-center">
                 <div className="flex gap-2 items-center">
                   <Avatar
@@ -83,18 +94,10 @@ export default function Home() {
                   <div className="flex flex-col">
                     <span className="text-small">{character.name}</span>
                     <span className="text-tiny text-default-400">
-                      {character.summary}
+                      {truncateString(character.summary || "")}
                     </span>
                   </div>
                 </div>
-                <Button
-                  className="border-small mr-0.5 font-medium shadow-small"
-                  radius="full"
-                  size="sm"
-                  variant="bordered"
-                >
-                  Add
-                </Button>
               </div>
             </AutocompleteItem>
           )}
